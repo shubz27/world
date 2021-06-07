0b4a19c37d7eb06af280f5fda0922b024f40911e
Shubham Dilip Jadhav <79862992+shubz27@users.noreply.github.com>
2021-06-07 17:53:00 +0530

    Add files via upload
    world program

---
+++ hello/world.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class world {
+	String a;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="shubham";
+System.out.println(a);
+
+	}
+
+}
